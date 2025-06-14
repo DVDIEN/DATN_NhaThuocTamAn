16f88dc65dd1fcc9d4d0f237d3b99415e74eef5b
DVDIEN <163952671+DVDIEN@users.noreply.github.com>
2025-06-14 18:09:35 +0700

    Add files via upload

---
+++ DATN_NhaThuocTamAn/DATN_NhaThuocTamAn/src/main/java/com/example/DATN_NhaThuocTamAn/DatnNhaThuocTamAnApplication.java
@@ -0,0 +1,13 @@
+package com.example.DATN_NhaThuocTamAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnNhaThuocTamAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnNhaThuocTamAnApplication.class, args);
+	}
+
+}
---
+++ DATN_NhaThuocTamAn/DATN_NhaThuocTamAn/src/test/java/com/example/DATN_NhaThuocTamAn/DatnNhaThuocTamAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DATN_NhaThuocTamAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnNhaThuocTamAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
